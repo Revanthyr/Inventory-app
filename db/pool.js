231731589cdec80907655f71f7df07a661354e9d
Revanthyr <155083567+Revanthyr@users.noreply.github.com>
2024-11-16 20:40:45 +0100

    more prod

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
-// TODO: Replace with dotenv
 const pool = new Pool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
   port: 5432,
   database: process.env.DATABASE_NAME,
+  ssl: "require",
 });
 module.exports = pool;
