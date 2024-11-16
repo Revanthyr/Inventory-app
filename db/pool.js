e20f3b22b7a6b9711c5053a6354829146866a315
Revanthyr <155083567+Revanthyr@users.noreply.github.com>
2024-11-16 21:27:52 +0100

    c

--- db/pool.js
+++ db/pool.js
@@ -1,16 +1,6 @@
 const { Pool } = require("pg");
-const { Client } = require("pg");
 
-const pool = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // Allow self-signed certificates
-  },
-});
-
-pool.connect();
-
-const a = new Pool({
+const pool = new Pool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
