4d65e10fe7b09dc0e29ac23c9bd1077eda9f82f9
Revanthyr <155083567+Revanthyr@users.noreply.github.com>
2024-11-16 21:15:20 +0100

    a

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,10 @@
 const { Pool } = require("pg");
+console.log({
+  user: process.env.DATABASE_USER,
 
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+});
 const pool = new Pool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
